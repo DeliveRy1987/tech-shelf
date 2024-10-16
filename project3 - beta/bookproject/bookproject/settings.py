826fcabee821b0b8ede434099c798ba35d5bfcde
Kensho <94515035+DeliveRy1987@users.noreply.github.com>
2024-10-16 23:18:14 +0900

    Add files via upload

--- project3 - beta/bookproject/bookproject/settings.py
+++ project3 - beta/bookproject/bookproject/settings.py
@@ -135,4 +135,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'funky0127@gmail.com'  # 送信用のGmailアドレス
-EMAIL_HOST_PASSWORD = 'pegx vhtv kpta aqnj'  # アプリパスワードを使うことを推奨
+EMAIL_HOST_PASSWORD = ''  
